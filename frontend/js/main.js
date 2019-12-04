b010833d18f9153394e486b883bc343045dba11b
itkreydo <it.kreydo@gmail.com>
2019-12-05 01:42:39 +0300

    bug fix

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,6 +2,8 @@ Vue.config.devtools = true;
 var app = new Vue({
     el: '#app',
     data: {
+        access: true,
+        login: '',
         name: "Регистрация",
         private_key: '',
         public_key: '',
@@ -15,7 +17,7 @@ var app = new Vue({
         
         key_generation(){
             this.private_key = "3cm84c8334uc3c48839cmm34cm34";
-            this.public_key = "rlrg455545g4gdfl45344bffgb5b"
+            this.public_key = "rlrg455545g4gdfl45344bffgb5b";
             
         },
 
