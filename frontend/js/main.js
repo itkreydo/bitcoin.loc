ce83f4ef51d935a76336581894d70c63d20451e4
Венера Гарифуллина <venera060698@yandex.ru>
2019-12-12 20:52:28 +0300

    start-server.bat and userpanel interface

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,17 +5,17 @@ var app = new Vue({
         access: true,
         login: '',
         name: "Регистрация",
-        private_key: '',
-        public_key: '',
+        private_key: 'c56c65c4w345v6b65evv45c54',
+        public_key: 'rtghtdreffr45g4g5j7i9ky7j7',
+        balance: '0.00567 BTC'
     },
     methods: {
-        
         enterAction(){
             window.location.href = "account.php";
             
         }, 
         
-        key_generation(){
+        generateKey(){
             this.private_key = "3cm84c8334uc3c48839cmm34cm34";
             this.public_key = "rlrg455545g4gdfl45344bffgb5b";
             
